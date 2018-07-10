a702a56e5ccddf7502f2db8b16d65b50d2fbd0ac
Alex Cowan <alexandria.cowan.17@tfacorps.org>
2018-07-10 19:25:18 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,35 +1,52 @@
 
 # Code your methods below
 
-def Assume_the_proper_stance
+def assume_the_proper_stance
   puts "Plant legs far apart, bend knees slightly, and keep posture loose"
+end
 
-def Base_footwork
+def base_footwork
   puts "life right foot"
   puts "return right foot to the ground"
   puts "finishing with a small skip-step backward"
+end
 
-def Skip_Step_Left
+def skip_Step_Left
   puts "lower left foot to the ground"
   puts "bounce left foot back up slightly, kicking it a few inches back"
   puts "Left, Right, Left and Left"
+end
   
-def Skip_Step_Right
+def skip_Step_Right
   puts "lower right foot to the ground"
   puts "bounce right foot back up slightly, kicking it a few inches back"
   puts "Right, Left, Right and Right"
+end
   
-def Bob_the_Reins
+def bob_the_Reins
   puts "Holding your arms out in front of you, straight and at chest level"
   puts "Cross your right writst over your left and hold them together"
   puts "Lift your arms up and down in a loose bouncing movement, in time with the beat of the song. This movement is repeated eight times"
+end
   
-def Lasso 
+def lasso 
   puts "Start arm movement by holding left arm so that your wrist is near your chin, pointin your left elbow straight left"
     puts "Lift right arm up and point right elbow diagonally to the right."
     puts "Raise right forearm so that it points straight up, and whit it in small circles to the beat of the song, as if you were a cowboy holding a rope lasso. This movement is repeated eight times as well."
+  end
     
-  def Bust_a_move
+  def bust_a_move
     puts "be cool"
     puts "be snazzy!"
+  end
+  
+def gangam_style
+  assume_the_proper_stance
+  base_footwork
+  skip_Step_Left
+  skip_Step_Right
+  bob_the_Reins
+  lasso
+  bust_a_move
+end
   
